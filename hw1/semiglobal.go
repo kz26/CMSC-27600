1909a371d86d6ee98c23cfbea19837036130f733
whitehat2k13 <whitehat2k9@gmail.com>
2013-04-12 04:30:49 -0500

    Fixed alignment output

--- hw1/semiglobal.go
+++ hw1/semiglobal.go
@@ -76,15 +76,15 @@ func getTraceback(m [][]int, a string, b string, i int, j int, alignmentA string
     y := getMax(0, j - 1)
     if i > 0 && j > 0 && m[i][j] == (m[i-1][j-1] + score(a[y], b[x])) {
         fmt.Println(m[i-1][j-1], "DIAG")
-        getTraceback(m, a, b, i-1, j-1, alignmentA + string(a[y]), alignmentB + string(b[x]))
+        getTraceback(m, a, b, i-1, j-1, string(a[y]) + alignmentA, string(b[x]) + alignmentB)
     }
     if i > 0 && m[i][j] == (m[i-1][j] + gp) {
         fmt.Println(m[i-1][j], "UP")
-        getTraceback(m, a, b, i-1, j, string(a[y]) + alignmentA, "-" + alignmentB)
+        getTraceback(m, a, b, i-1, j, "-" + alignmentA, string(b[x]) + alignmentB)
     }
     if j > 0 && m[i][j] == (m[i][j-1] + gp) {
         fmt.Println(m[i][j-1], "LEFT")
-        getTraceback(m, a, b, i, j-1, "-" + alignmentA, string(b[x]) + alignmentB)
+        getTraceback(m, a, b, i, j-1, string(a[y]) + alignmentA, "-" + alignmentB)
     }
 }
 
