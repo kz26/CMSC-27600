2e5619bd1807575ccc2b34a707654e2bbddf5e6e
whitehat2k13 <whitehat2k9@gmail.com>
2013-04-12 03:33:20 -0500

    Fixed up/left labeling; added semiglobal alignment

--- hw1/semiglobal.go
+++ hw1/semiglobal.go
@@ -32,18 +32,22 @@ func computeMatrix(a string, b string) [][]int {
     }
 
     for j := 0; j < len(a) + 1; j++ {
-        F[0][j] = j * GAP_PENALTY
+        F[0][j] = 0
     }
 
     for i := 0; i < len(b) + 1; i++ {
-        F[i][0] = i * GAP_PENALTY
+        F[i][0] = 0
     }
 
     for i := 1; i < len(b) + 1; i++ {
         for j := 1; j < len(a) + 1; j++ {
             match := F[i-1][j-1] + score(a[j-1], b[i-1])
-            delete := F[i-1][j] + GAP_PENALTY
-            insert := F[i][j-1] + GAP_PENALTY
+            delete := F[i-1][j]
+            insert := F[i][j-1]
+            if i != len(b) && j != len(a) {
+                delete += GAP_PENALTY 
+                insert += GAP_PENALTY
+            }
             F[i][j] = getMax(match, delete, insert)
         }
     }
@@ -62,17 +66,23 @@ func getTraceback(m [][]int, a string, b string, i int, j int, alignmentA string
         fmt.Println(alignmentA, alignmentB)
         return
     }
+    var gp int
+    if i == len(b) || j == len(a) || i == 0 || j == 0 {
+        gp = 0
+    } else {
+        gp = GAP_PENALTY
+    }
     x := getMax(0, i - 1)
     y := getMax(0, j - 1)
     if i > 0 && j > 0 && m[i][j] == (m[i-1][j-1] + score(a[y], b[x])) {
         fmt.Println(m[i-1][j-1], "DIAG")
         getTraceback(m, a, b, i-1, j-1, alignmentA + string(a[y]), alignmentB + string(b[x]))
     }
-    if i > 0 && m[i][j] == (m[i-1][j] + GAP_PENALTY) {
+    if i > 0 && m[i][j] == (m[i-1][j] + gp) {
         fmt.Println(m[i-1][j], "UP")
         getTraceback(m, a, b, i-1, j, string(a[y]) + alignmentA, "-" + alignmentB)
     }
-    if j > 0 && m[i][j] == (m[i][j-1] + GAP_PENALTY) {
+    if j > 0 && m[i][j] == (m[i][j-1] + gp) {
         fmt.Println(m[i][j-1], "LEFT")
         getTraceback(m, a, b, i, j-1, "-" + alignmentA, string(b[x]) + alignmentB)
     }
